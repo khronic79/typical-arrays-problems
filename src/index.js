bb8abdeee3ce1bc08c8ca35bfd00a0fa33f2188a
khronic79 <rustam.ptashinskiy@gmail.com>
2021-02-04 00:01:52 +0300

    init: make task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((arguments.length == 0) || (array.length == 0)) {return 0;}
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((arguments.length == 0) || (array.length == 0)) {return 0;}
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((arguments.length == 0) || (array.length == 0)) {return 0;}
+  let av = 0;
+  for (i = 0; i<array.length; i=i+1) {
+    av = av+array[i];
+  }
+  return av/array.length;
 }
